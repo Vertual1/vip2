d6a2f685e2c83475d126ee5d3aeace6396093e53
Vertual1 <hunanyandavid877@gmail.com>
2025-04-07 19:31:57 +0400

    commit chang

--- vip1/src/seeecret1.java
+++ vip1/src/seeecret1.java
@@ -1,5 +1,8 @@
 public class seeecret1 {
     public static void main(String[] args) {
         System.out.println("sdsadas");
+        int x =5, d=7, e=9;
+        int h =x+d+e;
+        System.out.println(h);
     }
 }
